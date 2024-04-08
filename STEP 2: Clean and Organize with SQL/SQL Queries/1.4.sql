c7c3cbc83297468e26d2916e65b386e0feb4025f
Vincent Daniel Holguin <145889072+V-Holguin@users.noreply.github.com>
2024-04-07 20:39:55 -0700

    Update 1.4.sql

--- STEP 2: Clean and Organize with SQL/SQL Queries/1.4.sql
+++ STEP 2: Clean and Organize with SQL/SQL Queries/1.4.sql
@@ -1,3 +1,4 @@
+-- Check for NULL values
 SELECT * FROM weather
 WHERE time_date IS NULL
 	OR tmpf IS NULL
@@ -6,16 +7,20 @@ WHERE time_date IS NULL
     OR wind_knt IS NULL
     OR sky IS NULL;
 
+-- Check 'tmpf' for non-numeric values
 SELECT tmpf
 FROM weather
 WHERE tmpf REGEXP '[^0-9.]';
 
+-- Delete rows with missing data
 DELETE FROM weather
 WHERE tmpf = 'M' OR dwpf = 'M' OR relh = 'M' OR wind_knt = 'M';
 
+-- Change the data type of the 'date_time' column to DATETIME
 ALTER TABLE weather
 MODIFY COLUMN date_time DATETIME;
 
+-- Change the data type of the numeric columns to DECIMAL with a precision of 5 and a scale of 2
 ALTER TABLE weather
 MODIFY COLUMN tmpf DECIMAL(5,2);
 
